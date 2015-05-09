e5665275d16da0c6b7688f8275d237301d83f714
Valentin Hübner <valentin.huebner@gmail.com>
2015-05-09 09:06:23 +0200

    first try

--- js/game_manager.js
+++ js/game_manager.js
@@ -4,8 +4,6 @@ function GameManager(size, InputManager, Actuator, StorageManager) {
   this.storageManager = new StorageManager;
   this.actuator       = new Actuator;
 
-  this.startTiles     = 2;
-
   this.inputManager.on("move", this.move.bind(this));
   this.inputManager.on("restart", this.restart.bind(this));
   this.inputManager.on("keepPlaying", this.keepPlaying.bind(this));
